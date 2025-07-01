f4e0e8a32ba9e992a9c9ce46f8fa08358961d586
SherryChaos <60814455+SherryChaos@users.noreply.github.com>
2025-07-01 16:42:11 +0800

    Add Android.mk build script for ocgcore
    Introduces an Android.mk file to build the ocgcore shared library with specified source files and dependencies, enabling Android NDK builds.

---
+++ Android.mk
@@ -0,0 +1,33 @@
+#Ocgcore: Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := ocgcore
+LOCAL_MODULE_FILENAME := libocgcore
+LOCAL_SRC_FILES := card.cpp \
+	duel.cpp \
+	effect.cpp \
+	field.cpp \
+	group.cpp \
+	interpreter.cpp \
+	libcard.cpp \
+	libdebug.cpp \
+	libduel.cpp \
+	libeffect.cpp \
+	libgroup.cpp \
+	mem.cpp \
+	ocgapi.cpp \
+	operations.cpp \
+	playerop.cpp \
+	processor.cpp \
+	scriptlib.cpp \
+	LuaMemTracker.cpp
+
+LOCAL_STATIC_LIBRARIES := lua
+
+LOCAL_CPPFLAGS := -std=c++14
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../lua/src
+
+include $(BUILD_SHARED_LIBRARY)
+
